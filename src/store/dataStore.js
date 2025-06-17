ac2723d5a9e04b0bf6d9fb403bd6256d30fc4fd7
waly-lin1 <zhenlei.lin@oceandigi.com.cn>
2025-06-17 19:49:08 +0800

    f

--- src/store/dataStore.js
+++ src/store/dataStore.js
@@ -9,16 +9,16 @@ export const useStore = defineStore('main', {
   state: () => ({
     isMobile: false,
     showMobileMenu: false,
-    domain:import.meta.env.VITE_APP_IMAGE_DOMAIN,
+    domain: import.meta.env.VITE_APP_IMAGE_DOMAIN,
     navLinks: [
-      { text: '首页', path: '/' , img:"@/assets/images/aa.png"},
-      { text: '公司优势', path: '/advantage' , img:"@/assets/images/aa.png"},
-      { text: '组织架构', path: '/structure' , img:"@/assets/images/aa.png"},
-      { text: '套餐及报价', path: '/product' , img:"@/assets/images/aa.png"},
-	  { text: '实时资讯', path: '/news' , img:"@/assets/images/aa.png"},
-	  { text: '关于我们', path: '/about' , img:"@/assets/images/aa.png"}
+      { text: '首页', path: '/', img: "@/assets/images/aa.png" },
+      { text: '公司优势', path: '/advantage', img: "@/assets/images/aa.png" },
+      { text: '组织架构', path: '/structure', img: "@/assets/images/aa.png" },
+      { text: '套餐及报价', path: '/product', img: "@/assets/images/aa.png" },
+      { text: '实时资讯', path: '/news', img: "@/assets/images/aa.png" },
+      { text: '关于我们', path: '/about', img: "@/assets/images/aa.png" }
     ],
-    config:{} // 用于存储从接口获取的配置
+    config: {} // 用于存储从接口获取的配置
   }),
   actions: {
     checkMobile() {
@@ -30,20 +30,34 @@ export const useStore = defineStore('main', {
     async fetchConfig() {
       // 尝试从 localStorage 中读取配置
       const storedConfig = localStorage.getItem('config');
+      const expirationTime = 24 * 60 * 60 * 1000; // 24小时，单位毫秒
+
       if (storedConfig) {
-        this.config = JSON.parse(storedConfig);
-        if(this.config?.official_email){
+        const parsedConfig = JSON.parse(storedConfig);
+        const { configData, timestamp } = parsedConfig;
+
+        // 判断是否未过期
+        if (configData?.official_email && Date.now() - timestamp < expirationTime) {
+          this.config = configData;
           return this.config;
-        }        
+        }
       }
 
       await commonApi.getConfigs().then((response) => {
-        this.config = response.data;
-        // 将获取到的配置存储到 localStorage
-        localStorage.setItem('config', JSON.stringify(this.config));
+        const newConfig = response.data;
+
+        // 构造带时间戳的数据
+        const configWithTimestamp = {
+          configData: newConfig,
+          timestamp: Date.now()
+        };
+
+        // 存入 localStorage
+        localStorage.setItem('config', JSON.stringify(configWithTimestamp));
+        this.config = newConfig;
       });
-      
-      return this.config            
+
+      return this.config
     }
   }
 })
